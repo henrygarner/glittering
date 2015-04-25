c2472a3bb4ef565d25ad3658a82deb3562ec261c
Henry Garner <henry@henrygarner.com>
2015-04-26 00:12:19 +0100

    Added clojure implementation of pregel and label propagation

--- src/clojure/shimmer/api.clj
+++ src/clojure/shimmer/api.clj
@@ -2,14 +2,14 @@
   (:require [sparkling.scalaInterop :as si]
             [sparkling.function :refer [function2]]
             [t6.from-scala.core :refer ($ $$) :as $])
-  (:import [org.apache.spark.graphx Edge EdgeDirection Graph GraphOps]
+  (:import [org.apache.spark.graphx Edge EdgeDirection Graph GraphOps Graph$]
            [scala.collection Iterator]
            [scala.reflect ClassTag]
            [scala.Predef$$eq$colon$eq$/MODULE$]
            [shimmer.scalaInterop ScalaFunction2 ScalaFunction3]))
 
 (defn edge [from to weight]
-  (new Edge from to weight))
+  ($ Edge from to weight))
 
 (defn edges [graph]
   (.edges graph))
@@ -22,6 +22,19 @@
 
 (defn map-edges [graph f]
   (.mapEdges graph (new sparkling.scalaInterop.ScalaFunction1 f) si/OBJECT-CLASS-TAG))
+
+(defn inner-join [rdd other f]
+  (.innerJoin rdd other
+              (new shimmer.scalaInterop.ScalaFunction3 f)
+              si/OBJECT-CLASS-TAG
+              si/OBJECT-CLASS-TAG))
+
+(defn outer-join-vertices [graph vertices f]
+  (.outerJoinVertices graph vertices
+                      (new shimmer.scalaInterop.ScalaFunction3 f)
+                      si/OBJECT-CLASS-TAG
+                      si/OBJECT-CLASS-TAG
+                      nil))
 
 (defn map-vertices [graph f]
   (.mapVertices graph
@@ -37,21 +50,60 @@
                                          (EdgeDirection/Out)))
                       si/OBJECT-CLASS-TAG))
 
-(defn graph [vertices edges]
+(defn aggregate-messages [graph send merge]
+  (.aggregateMessages graph
+                      (new sparkling.scalaInterop.ScalaFunction1 send)
+                      (new shimmer.scalaInterop.ScalaFunction2 merge)
+                      (.aggregateMessages$default$3 graph)
+                      si/OBJECT-CLASS-TAG))
+
+(defn graph [vertices edges default]
   ($ Graph
      (.rdd vertices)
-     (.rdd edges)
-     _ _ _ si/OBJECT-CLASS-TAG si/OBJECT-CLASS-TAG))
+     (.rdd edges) default
+     _ _ si/OBJECT-CLASS-TAG si/OBJECT-CLASS-TAG))
 
-(defn pregel [graph init vf sf mf]
+(defn graph-from-edges [edges default]
+  (.fromEdges Graph$/MODULE$ (.rdd edges) default
+              (.fromEdges$default$3 Graph$/MODULE$)
+              (.fromEdges$default$4 Graph$/MODULE$)
+              si/OBJECT-CLASS-TAG
+              si/OBJECT-CLASS-TAG))
+
+(defn pregel [graph init max vf sf mf]
   (println "PREGEL ##################")
-  (let [max 10
+  (let [
         dir (EdgeDirection/Either)
         v (new shimmer.scalaInterop.ScalaFunction3 vf)
         s (new sparkling.scalaInterop.ScalaFunction1 sf)
         m (new shimmer.scalaInterop.ScalaFunction2 mf)
         class-tag ($ ClassTag java.lang.Object)]
     (.pregel (.ops graph) init max dir v s m class-tag)))
+
+
+(defn pregel-impl [graph init max-iterations vf sf mf]
+  (let [dir (EdgeDirection/Either)
+        g (map-vertices graph (fn [vid attr]
+                                (vf vid attr init)))]
+    (loop [g g
+           messages (aggregate-messages g sf mf)
+           i 0]
+      (if (and (> (.count messages) 0)
+               (< i max-iterations))
+        (let [new-verts (.cache (inner-join (vertices g) messages vf))
+              old-g g
+              g (.cache (outer-join-vertices g new-verts
+                                             (fn [vid old new-opt]
+                                               ($ new-opt getOrElse ($/fn [] old)))))
+              old-messages messages
+              messages (.cache (aggregate-messages g sf mf))]
+          (println "Pregel iteration: " i "messages count: " (.count messages))
+          (.unpersist old-messages false)
+          (.unpersist new-verts false)
+          (.unpersistVertices old-g false)
+          (.unpersist (edges old-g) false)
+          (recur g messages (inc i)))
+        g))))
 
 (defn page-rank [graph tol reset-prob]
   (.pageRank (.ops graph) tol reset-prob))
--- src/clojure/shimmer/examples.clj
+++ src/clojure/shimmer/examples.clj
@@ -41,12 +41,7 @@
          (shimmer/map-edges (fn [e]
                               (double (.attr e))))
          (shimmer/map-vertices (fn [id value]
-                                 (if (= id 42)
-                                   0 100)))))))
-
-(defn vertex-program [id dist new-dist]
-  (println "VERTEX PROG" id dist new-dist)
-  new-dist)
+                                 0.0))))))
 
 #_(if (and (.srcAttr triplet)
            (.attr triplet)
@@ -55,15 +50,19 @@
     ($ Iterator & #sparkling/tuple[(.dstId triplet) (+ (.srcAttr triplet) (.attr triplet))])
     ($ Iterator/empty))
 
+(defn vertex-program [id weight message]
+  (println "VERTEX PROG" id weight message)
+  message)
+
 (defn send-message [triplet]
   (println "SEND MESSAGE")
   (println triplet)
   (println (class (.srcId triplet)))
-  ($ Iterator & ($/tuple (.dstId triplet) 10.0)))
+  ($ Iterator & ($/tuple (.dstId triplet) 1.0)))
 
 (defn merge-message [a b]
   (println "MERGE MESSAGE" a)
-  a)
+  (+ a b))
 
 (defn test-mr-triplets []
   (spark/with-context sc (-> (conf/spark-conf)
@@ -90,6 +89,72 @@
           (shimmer/map-vertices (fn [id value]
                                   (if (= id 42)
                                     0 100)))
-          (shimmer/pregel Double/POSITIVE_INFINITY vertex-program send-message merge-message)
+          (shimmer/pregel 0.0 1 vertex-program send-message merge-message)
           (.vertices)
-          (.first)))))
+          (spark/save-as-text-file "/tmp/vertices")))))
+
+(defn semi-clustering [graph]
+  (let [vfn (fn [vid attr message]
+              (println "Vertex" vid attr message)
+              (if (empty? message)
+                attr
+                (ffirst (sort-by second > message))))
+        sfn (fn [triplet]
+              (println "Triplet" triplet)
+              (let [src-id   (.srcId triplet)
+                    src-attr (.srcAttr triplet)
+;;                    src-attr (or src-attr src-id)
+                    dst-id   (.dstId triplet)
+                    dst-attr (.dstAttr triplet)
+;;                    dst-attr (or dst-attr dst-id)
+                    ]
+                ($ Iterator &
+                   ($/tuple src-id {src-attr 1})
+                   ($/tuple dst-id {dst-attr 1}))))
+        mfn (fn [a b]
+              (println "Merging" a b)
+              (merge-with + a b))
+        init {}
+        max 1]
+    (-> graph
+        (shimmer/map-reduce-triplets sfn mfn)
+        #_(shimmer/map-vertices (fn [vid attr] vid))
+        #_(shimmer/pregel init max vfn sfn mfn))))
+
+(defn test-connected-components [out]
+  (spark/with-context sc (-> (conf/spark-conf)
+                             (conf/master "local[*]")
+                             (conf/app-name "sparkling-test"))
+    (let [vfn (fn [vid attr message]
+                (println "##### Vertex" vid attr message)
+                (if (empty? message)
+                  attr
+                  (ffirst (sort-by second > message))))
+          sfn (fn [ctx]
+                (println "##### Send Message")
+                (let [src-id   (.srcId ctx)
+                      src-attr (.srcAttr ctx)
+                      dst-id   (.dstId ctx)
+                      dst-attr (.dstAttr ctx)                      ]
+                  (println src-id ":" src-attr " " dst-id ":" dst-attr)
+                  (.sendToSrc ctx {dst-attr 1})
+                  (.sendToDst ctx {src-attr 1})))
+          mfn (fn [a b]
+                (println "##### Merging" a b)
+                (merge-with + a b))
+          init {}
+          n 5
+          max 10
+          clique1 (for [u (range n)
+                        v (range n)]
+                    (shimmer/edge u v 1))
+          clique2 (for [u (range n)
+                        v (range n)]
+                    (shimmer/edge (+ u n) (+ v n) 1))
+          edges (spark/parallelize sc (concat clique1 clique2 [(shimmer/edge 0 n 1)]))]
+      (-> (shimmer/graph-from-edges edges 1)
+          (shimmer/map-vertices (fn [vid attr] vid)) 
+          (shimmer/pregel-impl init max vfn sfn mfn)
+          (.vertices)
+          (spark/save-as-text-file out)))))
+

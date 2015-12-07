09ec9b6e29ae92154ec2421e86ce380c90d7b90f
Henry Garner <henry@henrygarner.com>
2015-12-07 20:52:12 +0000

    Upgrade sparkling

--- project.clj
+++ project.clj
@@ -4,18 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [gorillalabs/sparkling "1.2.1"]
+                 [gorillalabs/sparkling "1.2.3"]
                  [t6/from-scala "0.2.1"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :aot [#".*" glittering.serialization sparkling.destructuring]
-  :profiles {:default [:base :system :user :provided :spark-1.3.1 :dev]
-             :spark-1.3.1 ^{:pom-scope :provided}
-             {:dependencies [[org.apache.spark/spark-core_2.11 "1.3.1"]
-                             [org.apache.spark/spark-graphx_2.11 "1.3.1"]]}
+  :profiles {:default [:base :system :user :provided :spark-1.4.0 :dev]
+             :spark-1.4.0 ^{:pom-scope :provided}
+             {:dependencies [[org.apache.spark/spark-core_2.10 "1.4.0"]
+                             [org.apache.spark/spark-graphx_2.10 "1.4.0"]]}
              :test {:resource-paths ["dev-resources"]
                     :aot [glittering.core
                           glittering.pregel
+                          glittering.destructuring
                           glittering.pregel-test
                           glittering.test-utils]}
              :dev {:resource-paths ["data"]
